059d97c872c76a7930155741456f6b2e7a4520af
Henrysoed <henry.soedibjo@gmail.com>
2025-03-04 21:45:34 +0700

    Tambahan: Implement double jump for character

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -3,23 +3,32 @@ extends CharacterBody2D
 @export var speed: int = 400
 @export var gravity: int = 1200
 @export var jump_speed: int = -400
+@export var max_jumps: int = 2  # Maksimum lompatan (termasuk double jump)
 
+var jump_count: int = 0  # Melacak jumlah lompatan yang telah dilakukan
 
 func get_input():
 	velocity.x = 0
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
+
+	# Reset jump_count jika karakter menyentuh lantai
+	if is_on_floor():
+		jump_count = 0
+
+	# Jika tombol lompat ditekan dan masih ada lompatan yang tersisa
+	if Input.is_action_just_pressed("jump") and jump_count < max_jumps:
 		velocity.y = jump_speed
+		jump_count += 1  # Tambah jumlah lompatan
+
+	# Gerakan kiri/kanan
 	if Input.is_action_pressed("right"):
 		velocity.x += speed
 	if Input.is_action_pressed("left"):
 		velocity.x -= speed
 
-
 func _physics_process(delta):
 	velocity.y += delta * gravity
 	get_input()
 	move_and_slide()
-
 
 func _process(_delta):
 	if not is_on_floor():
@@ -29,8 +38,6 @@ func _process(_delta):
 	else:
 		$Animator.play("Idle")
 
+	# Flip sprite berdasarkan arah gerakan
 	if velocity.x != 0:
-		if velocity.x > 0:
-			$Sprite2D.flip_h = false
-		else:
-			$Sprite2D.flip_h = true
+		$Sprite2D.flip_h = velocity.x < 0
