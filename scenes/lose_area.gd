4bbac386d372e3ed88318149e0e33d8b9cceea14
Henrysoed <henry.soedibjo@gmail.com>
2025-03-17 13:03:36 +0700

    Tutorial 6: Latihan membuat menu utama dan clickable menu

--- scenes/lose_area.gd
+++ scenes/lose_area.gd
@@ -1,7 +1,16 @@
 extends Area2D
 
-@export var sceneName: String = "LoseScreen"
+@export var scene_name: String = "Level1"
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.get_name() == "Player":
-		get_tree().change_scene_to_file(str("res://scenes/" + sceneName + ".tscn"))
+	var current_scene_path = get_tree().current_scene.scene_file_path
+	var current_scene_name = current_scene_path.get_file().trim_suffix(".tscn")
+
+	if body.name == "Player":
+		if current_scene_name == scene_name:
+			global.lives -= 1
+		
+		if global.lives == 0:
+			get_tree().change_scene_to_file(str("res://scenes/LoseScreen.tscn")) 
+		else:
+			get_tree().change_scene_to_file("res://scenes/" + scene_name + ".tscn")
